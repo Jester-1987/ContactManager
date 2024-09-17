762c63c4df1e2af07aa92b7ecb999b9f44a0004d
Jester-1987 <nicole.groeger@triosstudent.com>
2024-09-17 10:57:07 -0400

    Added database.php

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    require("database.php")
+?>
+    <!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Manager - Home</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
 </head>
 <body>
-    <?php include("header.php");?>
+    <?php include("header.php"); ?>
     <main>
+
     </main>
-    <?php include("footer.php");?>
+    <?php include("footer.php"); ?>
 </body>
 </html>
